2060c19f85b9990da3f778bb883adbaff4521e1f
David Cohan <davidcohan@bfaindustries.com>
2021-11-04 12:52:49 -0300

    [main] tmp1

--- js/background.js
+++ js/background.js
@@ -1,7 +1,14 @@
-/*
-const AESR_ExtensionId = "xxxxxxxxxxxxxx";
+const jarvisExtensionId = "jpmkfafbacpgapdghgdpembnojdlgkdl";
+const url = "https://aws-resource-finder-api.corp.ipsy.com/v0/account"
+fetch(url).then(function(response) {
+  return response.json();
+}).then(function(data) {
+  console.log(data);
+}).catch(function() {
+  console.log("Booo");
+});
 
-  const rawIniStr = `
+const rawIniStr = `
 [profile marketingadmin]
 role_arn = arn:aws:iam::123456789012:role/marketingadmin
 color = ffaaee
@@ -17,11 +24,17 @@ role_name = athirdrole
 image = "https://via.placeholder.com/150"
 `;
 
-  chrome.runtime.sendMessage(AESR_ExtensionId, {
-      action: 'updateConfig',
-      dataType: 'ini',
-      data: rawIniStr
-    }, function(response) {
-      console.log(response)
-    });
-*/
+chrome.runtime.sendMessage(jarvisExtensionId, {
+  action: 'updateConfig',
+  dataType: 'ini',
+  data: rawIniStr
+}, function(response) {
+  console.log(response)
+});
+
+
+async function fetchAsync (url) {
+  let response = await fetch(url);
+  let data = await response.json();
+  return data;
+}
